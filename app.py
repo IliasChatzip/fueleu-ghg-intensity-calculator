2620a16c9fc1f7c9e312938533131a59bbe916ab
IliasChatzip <iliaschatzipa@gmail.com>
2025-05-23 19:55:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ for category, fuels_in_cat in categories.items():
         for selected_fuel in selected_fuels:
             qty = st.number_input(f"{selected_fuel} (t)", min_value=0, step=1, value=0, format="%d", key=f"qty_{selected_fuel}")
             fuel_inputs[selected_fuel] = qty
-            row["Price (Eur/t)"] = st.number_input(f"{row['Fuel']} - Price (Eur/t)",min_value=0.0,value=0.0,step=10.0,key=f"mit_price_{row['Fuel']}_mit")
+            price = st.number_input(f"{selected_fuel} - Price (Eur/t)",min_value=0.0,value=0.0,step=10.0,key=f"price_{selected_fuel}")
+            fuel_price_inputs[selected_fuel] = price
 
 
 st.sidebar.markdown("---")
