3582941fc9e55c6349ff3dc8d87d395f4ed259b4
IliasChatzip <iliaschatzipa@gmail.com>
2025-06-17 12:56:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -659,14 +659,14 @@ if st.button("Export to PDF"):
             pdf.set_font("Arial", style="B", size=11)
             pdf.cell(200, 10, txt=f"- Initial fuels + Penalty: N/A (missing prices)", ln=True)
         
-        if total_cost > 0:
+        if total_cost and pooling_price_usd_per_tonne > 0:
             pdf.set_font("Arial", style="B", size=11)
             pdf.cell(200, 10, txt=f"- Initial fuels + Pooling, no Penalty: {total_with_pooling:,.2f} Eur", ln=True)
         else:
             pdf.set_font("Arial", style="B", size=11)
             pdf.cell(200, 10, txt=f"- Initial fuels + Pooling, no Penalty: N/A (missing prices)", ln=True)
         
-        if total_cost > 0:
+        if total_cost and price_usd > 0:
             pdf.set_font("Arial", style="B", size=11)
             pdf.cell(200, 10, txt=f"- Initial fuels + Bio fuels, no Penalty: {total_with_mitigation:,.2f} Eur", ln=True)                        
         else:
