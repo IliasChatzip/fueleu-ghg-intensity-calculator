6855e0367f79122a5534de7a822502eb342fd0d0
Ilias Chatzip <iliaschatzipa@gmail.com>
2025-07-07 12:16:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -589,8 +589,9 @@ if st.button("Export to PDF"):
     else:
         pdf = FPDF()
         pdf.add_page()
-        pdf.set_font("Arial", style="BU" ,size=11)
+        pdf.set_font("Arial", style="BU" ,size=12)
         pdf.cell(200, 10, txt="Fuel EU Maritime GHG & Penalty Report", ln=True, align="C")
+        pdf.set_font("Arial", "B" ,size=11)
         pdf.cell(200, 10, txt=f"Year: {year} | GWP: {gwp_choice}", ln=True)
         pdf.cell(200, 10, txt=f"EU Target for {year}: {target_intensity(year):.2f} gCO2eq/MJ", ln=True)
         pdf.cell(200, 10, txt=f"GHG Intensity: {ghg_intensity:.2f} gCO2eq/MJ", ln=True)
@@ -601,8 +602,8 @@ if st.button("Export to PDF"):
         pdf.ln(10)
 
         # Fuel Breakdown
-        pdf.set_font("Arial", "U" ,size=10)
-        pdf.cell(200, 10, txt="Fuel Breakdown", ln=True)
+        pdf.set_font("Arial", size=10)
+        pdf.cell(200, 10, txt="Fuel Breakdown:", ln=True)
         user_entered_prices = any(fuel_price_inputs.get(f["name"], 0.0) > 0.0 for f in FUELS)
         for row in rows:
             fuel_name = row['Fuel']
@@ -666,7 +667,7 @@ if st.button("Export to PDF"):
         if total_cost > 0:
             pdf.ln(5)
             pdf.set_font("Arial", "B", size=12)
-            pdf.cell(200, 10, txt="--- Cost Analysis ---", ln=True)
+            pdf.cell(200, 10, txt="--- Cost-Benefit Analysis ---", ln=True)
             pdf.set_font("Arial", size=10)
         if total_cost and eua_ets_price > 0:
             pdf.set_font("Arial", style="B", size=11)
